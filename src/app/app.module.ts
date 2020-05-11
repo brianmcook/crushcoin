e433c13e256a1e87ae2a7a2946a9d80d50d5a0f2
Brian M. Cook <32991181+brianmcook@users.noreply.github.com>
2020-05-11 16:46:28 -0400

    Added bootstrap to angular
    - app.module.ts by adding angular flex layout and cdk.  updated node modules as well, so will need to do taht on production server.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
   imports: [
     BrowserModule,
+    FlexLayoutModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule    
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [] 
 })
 export class AppModule { }
