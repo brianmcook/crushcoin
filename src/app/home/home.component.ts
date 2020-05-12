2a71b63f9f7f5058e7cd7fefe25bc27c72c48e0b
Brian M. Cook <32991181+brianmcook@users.noreply.github.com>
2020-05-12 02:28:16 -0400

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,7 +3,6 @@ import { Router } from "@angular/router";
 
 @Component({
     selector: "app-home",
-    moduleId: module.id,
     templateUrl: "./home.component.html",
     styleUrls: ["./home.component.scss"]
 })
