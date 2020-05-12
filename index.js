6cda9a96d644c74592138bdf60159c0862a69a26
Brian M. Cook <32991181+brianmcook@users.noreply.github.com>
2020-05-12 02:35:48 -0400

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 4201;
-app.get('/', (req, res) => res.send('crushcoin-app'));
-app.listen(port, () => console.log(`crushcoin-app listening on port ${port}!`))
