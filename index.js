000a2c7b45d3b930108f9f963a3e0dd3594886fc
Brian M. Cook <32991181+brianmcook@users.noreply.github.com>
2020-05-11 22:41:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = express();
 const port = 4201;
 /* app.get('/', (req, res) => res.send('crushcoin-app')); */
-app.all('*', function (req, res) {res.status(200).sendFile (`/`, {root: ./app});});
+app.all('*', function (req, res) {res.status(200).sendFile (`/`, {root: _app_folder});});
 app.listen(port, () => console.log(`crushcoin-app listening on port ${port}!`))
